f184a482716a5e99f05084c14ee01682b6ad512c
Hokuto <abrahamhokuto@outlook.com>
2018-08-08 12:32:36 +0900

    login; publish threads

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,8 @@ import misaka
 import html
 import re
 import hashlib
+import datetime
+import uuid
 
 with open("config.json") as f:
     config = json.load(f)
@@ -14,24 +16,39 @@ app.jinja_env.line_statement_prefix = "#"
 app.jinja_env.line_comment_prefix = "///"
 
 def get_mysql_connection():
-    return mysql.connector.connect(pool_name = "kagerou", **config["db"])
+    return mysql.connector.connect(pool_name = "kagerofu", **config["db"])
 
 def render_template(template, **kwargs):
     args = {
         "title": "How do your end up here?",
         "body": "How do your end up here?"
     }
-        
+
+    try:
+        cookie = read_cookie(flask.request.cookies["session"])
+    except KeyError:
+        cookie = None
+
     try:
         cnx = get_mysql_connection()
         cursor = cnx.cursor()
         cursor.execute('SELECT name, HEX(id) FROM Category ORDER BY id')
         categories = list(cursor)
 
+        cursor = cnx.cursor()
+        cursor.execute('SELECT name, email FROM User WHERE id = UNHEX(%s)', (cookie, ))
+        try:
+            user, avatar = cursor.next()
+            avatar = hashlib.md5(avatar.strip().lower().encode("utf8")).hexdigest()
+        except StopIteration:
+            user, avatar = None, None
+
     finally:
-        cnx.close()
+        cnx.close()    
 
     kwargs["categories"] = categories
+    kwargs["user"] = user
+    kwargs["avatar"] = avatar
 
     return flask.render_template(template, **kwargs)
 
@@ -60,7 +77,7 @@ def render_content(renderer, content):
         content = html.escape(content)
         lines = content.split("\n")
         lines = map(lambda x: "<p>{}</p>".format(x), lines)
-        return lines.join("")
+        return "".join(lines)
 
     if renderer == "markdown":
         return renderer_markdown(content)
@@ -72,10 +89,13 @@ def create_cookie(user):
     cookie = "{}|{}".format(user, cookie_hash)
     return cookie
 
-def test_cookie(cookie):
-    user, hash = cookie.rsplit('|')
+def read_cookie(cookie):
+    user, hashval = cookie.rsplit('|')
     cookie_hash = hashlib.sha256((user + config["cookie_key"]).encode("utf8")).hexdigest()
-    return hashlib.sha256((user + config["cookie_key"]).encode("utf8")).hexdigest() == hash
+    if hashlib.sha256((user + config["cookie_key"]).encode("utf8")).hexdigest() == hashval:
+        return user
+    else:
+        return None
         
 def list_threads(order, page, category = None):
     order_sql = {
@@ -100,8 +120,8 @@ def list_threads(order, page, category = None):
     page_query = "SELECT COUNT(*) FROM Thread WHERE hidden = FALSE AND draft = FALSE " + category_sql
     category_query = "SELECT name FROM Category WHERE id = UNHEX(%s)"
 
+    cnx = mysql.connector.connect(pool_name = "kagerou", **config["db"])
     try:
-        cnx = mysql.connector.connect(pool_name = "kagerou", **config["db"])
         cursor = cnx.cursor(buffered=True)
         page_cursor = cnx.cursor(buffered=True)
 
@@ -141,8 +161,8 @@ def category_list(category, order, page):
 
 @app.route('/thread/view/<thread>/<page>')
 def post(thread, page):
+    cnx = get_mysql_connection()
     try:
-        cnx = get_mysql_connection()
         cursor = cnx.cursor()
         cursor.execute("SELECT title FROM Thread WHERE id = UNHEX(%s)", (thread, ))
         title = cursor.next()[0]
@@ -178,4 +198,152 @@ def post(thread, page):
                            page = int(page), total_pages = total_pages,
                            thread_author_id = thread_author_id,
                            baseurl = "/thread/view/{}".format(thread), post_per_page = config["paginator"]["post_per_page"],
-                           render_content = render_content, title = title)
+                           render_content = render_content, title = title, thread_id = thread)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if flask.request.method == "GET":
+        referrer = flask.request.referrer
+    else:
+        referrer = flask.request.form["referrer"]
+
+    try:
+        referrer.index("/login", title = "Login")
+    except ValueError:
+        pass
+    else:
+        referrer = "/"
+
+    if flask.request.method == "GET":        
+        return render_template("login.tmpl", title = "Login", referrer = flask.request.referrer, error = None)
+    else:
+        username = flask.request.form["username"]
+        password = flask.request.form["password"]
+
+        cnx = get_mysql_connection()
+        try: 
+            cursor = cnx.cursor()
+            cursor.execute("SELECT HEX(id) AS uid FROM User WHERE name = %s AND password = UNHEX(SHA2(%s, 256))",
+                           (username, password))
+            try:
+                userid = cursor.next()[0]
+            except StopIteration:
+                error = "Wrong username or password"
+                return render_template("login.tmpl", referrer = referrer, error = error)
+        finally:
+            cnx.close()
+
+        cookie = create_cookie(userid)
+        response = flask.make_response(flask.redirect(referrer))
+        response.set_cookie("session", cookie, expires=32503680000)
+        return response
+
+@app.route('/logout')
+def logout():
+    referrer = flask.request.referrer
+
+    print(referrer)
+    try:
+        referrer.index("/logout")
+    except ValueError:
+        pass
+    else:
+        referrer = "/"
+
+    print(referrer)
+
+    response = flask.make_response(flask.redirect(referrer))
+    response.set_cookie("session", "", expires=0)
+    return response
+
+@app.route('/new')
+def new():
+    cnx = get_mysql_connection()
+    try:
+        cursor = cnx.cursor()
+        cursor.execute("SELECT HEX(id), name FROM Category")
+        category_list = list(cursor)
+        
+    finally:
+        cnx.close()
+
+    return render_template("new.tmpl", title = "New Thread", categories = category_list)
+
+@app.route('/action/new_thread', methods=['POST'])
+def new_thread():
+    now = datetime.datetime.now()
+
+    try:
+        userid = read_cookie(flask.request.cookies["session"])
+    except KeyError:
+        flask.abort(401)
+
+    if userid == None:
+        flask.abort(401)
+
+    title = flask.request.form["title"]
+    category = flask.request.form["category"]
+    renderer = flask.request.form["renderer"]
+    content = flask.request.form["content"]
+    is_draft = bool(flask.request.form["draft"])
+
+    thread_id = str(uuid.uuid4()).replace('-', '')
+    post_id = str(uuid.uuid4()).replace('-', '')
+    post_content_id = str(uuid.uuid4()).replace('-', '')
+
+    cnx = get_mysql_connection()    
+    try:
+        cursor = cnx.cursor()
+        cursor.execute("INSERT INTO Thread VALUES ("
+                       "UNHEX(%s), UNHEX(%s), UNHEX(%s), %s, %s, FALSE, %s)",
+                       (thread_id, userid, category, now, title, is_draft))
+        
+        cursor.execute("INSERT INTO Post VALUES ("
+                       "UNHEX(%s), UNHEX(%s), UNHEX(%s), %s, FALSE, %s, UNHEX(%s))",
+                       (post_id, userid, thread_id, now, now, post_content_id))
+
+        cursor.execute("INSERT INTO PostContent VALUES ("
+                       "UNHEX(%s), UNHEX(%s), UNHEX(%s), %s, %s, %s)",
+                       (post_content_id, post_id, userid, content, renderer, now))
+        cnx.commit()
+
+    finally:
+        cnx.close()
+
+    return flask.redirect("/thread/view/" + thread_id)
+
+@app.route('/action/reply', methods=['POST'])
+def reply():
+    now = datetime.datetime.now()
+
+    try:
+        userid = read_cookie(flask.request.cookies["session"])
+    except KeyError:
+        flask.abort(401)
+
+    if userid == None:
+        flask.abort(401)
+
+    renderer = flask.request.form["renderer"]
+    content = flask.request.form["content"]
+    thread_id = flask.request.form["thread_id"]
+
+    post_id = str(uuid.uuid4()).replace('-', '')
+    post_content_id = str(uuid.uuid4()).replace('-', '')
+
+    cnx = get_mysql_connection()
+    try:
+        cursor = cnx.cursor()
+        cursor.execute("INSERT INTO Post VALUES ("
+                       "UNHEX(%s), UNHEX(%s), UNHEX(%s), %s, FALSE, %s, UNHEX(%s))",
+                       (post_id, userid, thread_id, now, now, post_content_id))
+        cursor.execute("INSERT INTO PostContent VALUES ("
+                       "UNHEX(%s), UNHEX(%s), UNHEX(%s), %s, %s, %s)",
+                       (post_content_id, post_id, userid, content, renderer, now))
+        cnx.commit()
+    finally:
+        cnx.close()
+
+    return flask.redirect(flask.request.referrer)
+
+
